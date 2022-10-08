0593283573b8ce04bd09662ae61e43a31ca16978
Kunjan Vaghela <kunjan.vaghela@rediff.com>
2022-10-08 11:27:40 -0400

    AStar working

--- Maze/main.py
+++ Maze/main.py
@@ -1,14 +1,15 @@
 from calendar import c
 from glob import glob
-from queue import Empty
+from queue import Empty, PriorityQueue
+from types import CellType
 import numpy as np
 import time
 
 # Variable Declare
 my_grid=[]              # To store grid
 invalid_indices=[]      # To store indices which are blocked, and where ghost cannot pop up
-grid_size=10            # Size of the grid
-nr_of_ghosts=10          # Number of the ghosts to conjure
+grid_size=6            # Size of the grid
+nr_of_ghosts=8          # Number of the ghosts to conjure
 
 # To create the grid
 def create_grid(grid_size, blocked_cell=0.28):
@@ -135,7 +136,100 @@ def depth_first_search(my_grid, goal_x, goal_y):
         print('No path present')
         print(error111)
         return False
-        
+
+def h(cell1, cell2):
+    x1,y1 = cell1       # Represents cell1 coordinates
+    x2,y2 = cell2       # Represents cell2 coordinates
+    return abs(x1-x2) + abs(y1-y2)      # Maanhattan distance between cell1 and cell2
+
+def aStar(gridd, start_x=0, start_y=0, goal_x=grid_size-1, goal_y=grid_size-1):
+    start=(start_x,start_y)
+    gridPos=[]
+    a=0
+    for i in gridd:
+        for j in range(len(i)):
+            gridPos.append((a,j))
+        a=a+1
+    print('gridPos = '+str(gridPos))
+    g_score={cell: float('inf') for cell in gridPos}
+    g_score[start]=0
+    f_score={cell: float('inf') for cell in gridPos}
+    f_score[start] = h(start, (goal_x, goal_y))     # Heuristic cost of start cell + g_score of the start cell. Since g_score of start cell is 0, only heuristic cost is assigned
+    print('g_score : ' + str(g_score))
+    print('f_score : ' + str(f_score))
+    openQueue = PriorityQueue()             # Assigned Priority Queue
+    openQueue.put(((h(start, (goal_x, goal_y)) + 0), h(start, (goal_x, goal_y)), start))      # Priority Queue contains tuple in order: 1) Heuristic Cost + g_score, 2) Heuristic cost, and 3) start cell
+    aPath = {}
+    #print('openQueue : '+str(openQueue.get()))
+    while not openQueue.empty():
+        currCell = openQueue.get()[2]           # Cell value in the Priority Queue is selected as the current cell for this loop
+        print('currCell : '+ str(currCell))
+        if currCell == (goal_x,goal_y):
+            print('currCell reached to goalCell : '+str(currCell))
+            break
+        # write code to check each side : LUDR and check if not going array out of bounds
+        # Code to select nextCell as Left
+        if currCell[1] != 0:
+            if (my_grid[currCell[0], (currCell[1] - 1)] % 10) == 0:
+                nextCell = (currCell[0], (currCell[1] - 1))
+                g1 = g_score[currCell] + 1
+                f1 = g1 + h(nextCell , (goal_x, goal_y))
+                if f1 < f_score[nextCell]:
+                    g_score[nextCell] = g1
+                    f_score[nextCell] = f1
+                    openQueue.put((f1, h(nextCell,(goal_x,goal_y)), nextCell))
+                    aPath[nextCell] = currCell
+                print('In Left loop')
+        # Code to select nextCell as Up
+        if currCell[0] != 0:
+            if (my_grid[(currCell[0] - 1), currCell[1]] % 10) == 0:
+                nextCell = ((currCell[0] - 1), currCell[1])
+                g1 = g_score[currCell] + 1
+                f1 = g1 + h(nextCell , (goal_x, goal_y))
+                if f1 < f_score[nextCell]:
+                    g_score[nextCell] = g1
+                    f_score[nextCell] = f1
+                    openQueue.put((f1, h(nextCell,(goal_x,goal_y)), nextCell))
+                    aPath[nextCell] = currCell
+                print('In Up loop')
+        # Code to select nextCell as Down
+        if currCell[0] != (grid_size - 1):
+            if (my_grid[currCell[0] + 1, currCell[1]] % 10) == 0:
+                nextCell = ((currCell[0] + 1), currCell[1])
+                g1 = g_score[currCell] + 1
+                f1 = g1 + h(nextCell , (goal_x, goal_y))
+                print('f1 : '+str(f1)+'; type:'+ str(type(f1)))
+                print('f_score : '+str(f_score)+'; type:'+ str(type(f_score)))
+                print(f_score[nextCell])
+                print('f_score[nextCell] : '+str(f_score[nextCell])+'; type:'+ str(type(f_score[nextCell])))
+                if f1 < f_score[nextCell]:
+                    g_score[nextCell] = g1
+                    f_score[nextCell] = f1
+                    openQueue.put((f1, h(nextCell,(goal_x,goal_y)), nextCell))
+                    aPath[nextCell] = currCell
+                print('In Down loop')
+        # Code to select nextCell as Right
+        if currCell[1] != (grid_size - 1):
+            if (my_grid[currCell[0], currCell[1] + 1] % 10) == 0:
+                nextCell = (currCell[0], (currCell[1] + 1))
+                print(nextCell)
+                g1 = g_score[currCell] + 1
+                f1 = g1 + h(nextCell , (goal_x, goal_y))
+                if f1 < f_score[nextCell]:
+                    g_score[nextCell] = g1
+                    f_score[nextCell] = f1
+                    openQueue.put((f1, h(nextCell,(goal_x,goal_y)), nextCell))
+                    aPath[nextCell] = currCell
+                print('In Right loop')
+    print(openQueue)
+    print('aPath : '+str(aPath))
+    fwdPath={}
+    goal_xy=(goal_x,goal_y)
+    while goal_xy!=start:
+        fwdPath[aPath[goal_xy]] = goal_xy
+        goal_xy = aPath[goal_xy]
+    print('fwdPath : '+ str(fwdPath))
+
 
 def create_env():
     global my_grid, invalid_indices
@@ -147,3 +241,4 @@ def create_env():
     # print(my_grid)
 
 create_env()
+aStar(my_grid)
